86b871c81ab2ce8b46f0099a85e10a73204503cd
Rodrigo Erickson <rodrigo.erickson@gmail.com>
2020-08-12 23:31:21 -0300

    Consumindo total de conexões para exibir na lading page

--- web/src/pages/landing/index.tsx
+++ web/src/pages/landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -7,8 +7,19 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css'
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 function Landing() {
+
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(()=>{
+        api.get('connections').then(response => {
+            const {total} = response.data; 
+            setTotalConnections(total);
+        })
+    }, [])
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -31,7 +42,7 @@ function Landing() {
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                    Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </span>
 
             </div>
